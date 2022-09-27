53d48b7d54748ddca8879a8375f2e0409662177a
Olha Yarosh <yarosholha1@gmail.com>
2022-09-28 00:17:18 +0300

    Created Menu and Main Page.

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './MainPage.module.css'
+
+const MainPage = () => {
+  return (
+    <div className={classes.mainPageBlock}>
+        <div className={classes.array}></div>
+    </div>
+  )
+}
+
+export default MainPage
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './Menu.module.css'
+
+const Menu = () => {
+  return (
+    <div className={classes.menuBlock}>
+        <div className={classes.titleMenu}>Menu</div>
+        <div className={classes.selection}>Reverse Array</div>
+    </div>
+  )
+}
+
+export default Menu
