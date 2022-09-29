579a79e6068d32b382852ca084550b552268ec00
Olha Yarosh <yarosholha1@gmail.com>
2022-09-29 12:30:29 +0300

    Text array blocks.

--- src/components/MainPage/ReverseArray/ArrayBlock/ArrayBlock.js
+++ src/components/MainPage/ReverseArray/ArrayBlock/ArrayBlock.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './ArrayBlock.module.css';
 
 const ArrayBlock = () => {
+
+    const [arrayList, setArrayList] = useState([
+        {text: 'Hello'}
+    ]);
     return (
-        <div className={classes.blockArray} id='blockArray'></div>
+        <div className={classes.blockArray} >
+            {arrayList.map(array=>
+                <div 
+                    id='blockArray'>
+                    {array.text}
+                </div>
+            )}
+        </div>
     )
 }
 
