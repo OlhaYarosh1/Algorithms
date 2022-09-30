8d717883e20c34c42527e3fd60470e42adea0280
Olha Yarosh <yarosholha1@gmail.com>
2022-09-30 10:50:38 +0300

    fixed the problems.

--- src/components/MainPage/ReverseArray/ArrayBlock/ArrayBlock.js
+++ src/components/MainPage/ReverseArray/ArrayBlock/ArrayBlock.js
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classes from './ArrayBlock.module.css';
 
 const ArrayBlock = () => {
-
-    const [arrayList, setArrayList] = useState([
-        {text: 'Hello'}
-    ]);
     return (
-        <div className={classes.blockArray} >
-            {arrayList.map(array=>
-                <div 
-                    id='blockArray'>
-                    {array.text}
-                </div>
-            )}
+        <div className={classes.blockArray}>
+
         </div>
     )
 }
--- src/components/MainPage/ReverseArray/ReverseArray.js
+++ src/components/MainPage/ReverseArray/ReverseArray.js
@@ -4,18 +4,19 @@ import classes from './ReverseArray.module.css';
 
 const ReverseArray = () => {
 
-    // let array = [1, 2, 3, 4, 5, 6];
+    let array = [1, 2, 3, 4, 5, 6];
 
-    // function reverse(array) {
-    //     let res = [];
-    //     for(let i = array.length-1; i>=0; i--) {
-    //         res.push(array[i]);
-    //     }
-    //     return res;
-    // }
+    function reverse(array) {
+        let res = [];
+        for(let i = array.length - 1; i >=0; i--) {
+            res.push(array[i]);
+        }
+        return res;
+    }
 
-    // console.log(reverse(array));
-    // console.log(array[0]);
+
+    console.log(reverse(array));
+    //console.log(array[0]);
 
     // document.querySelector('randomArray').onclick = function() {
     //     let result = [];
@@ -32,21 +33,23 @@ const ReverseArray = () => {
     //     return rand;
     // }
 
+
+    const getArrayContent = () => {
+        return (array.map((item, index) => {
+            return (<ArrayBlock value={item} key={index} />)
+        }));
+    }
+
     return (
         <div className={classes.reverseArrayContainer}>
             <div className={classes.blockArrayContainer}>
-                <ArrayBlock />
-                <ArrayBlock />
-                <ArrayBlock />
-                <ArrayBlock />
-                <ArrayBlock />
-                <ArrayBlock />
+                {getArrayContent()}
             </div>
             <div className={classes.randomButtonContainer}>
                 <button className={classes.randomArray}>Random Array</button>
             </div>
             <div className={classes.reverseButtonContainer}>
-                <button className={classes.reverseArray}>Reverse Array</button>
+                <button className={classes.reverseArray} onClick={reverse}>Reverse Array</button>
             </div>
         </div>
     )
