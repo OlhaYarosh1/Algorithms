28ea2561069532cf9c00bdf56b443830cb0e0c79
Olha Yarosh <yarosholha1@gmail.com>
2022-09-30 12:21:24 +0300

    Added values ​​to array blocks.

--- src/components/MainPage/ReverseArray/ArrayBlock/ArrayBlock.js
+++ src/components/MainPage/ReverseArray/ArrayBlock/ArrayBlock.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from './ArrayBlock.module.css';
 
-const ArrayBlock = () => {
+const ArrayBlock = ({value}) => {
     return (
         <div className={classes.blockArray}>
-
+            {value}
         </div>
     )
 }
