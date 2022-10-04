3afebc90e470729aafba3f1c7529826f2467a9cd
Olha Yarosh <yarosholha1@gmail.com>
2022-10-05 01:43:03 +0300

    Added functions to randomise ans reverse array.

--- src/components/MainPage/ReverseArray/ReverseArray.js
+++ src/components/MainPage/ReverseArray/ReverseArray.js
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArrayBlock from './ArrayBlock/ArrayBlock';
 import classes from './ReverseArray.module.css';
 
 const ReverseArray = () => {
 
-    let array = [1, 2, 3, 4, 5, 6];
+    const [arrayRan, setArrayRan] = useState([]);
 
-    function reverse(array) {
-        let res = [];
-        for(let i = array.length - 1; i >=0; i--) {
-            res.push(array[i]);
-        }
-        return res;
+    const handleOnClickRandom = () => {
+        const randomArray = random(arrayRan);
+        function random() {
+            const range = 100;
+            const count = 5;
+            let m = {};
+            let a = [];
+            for (let i = 0; i < count; i++) {
+                let r = Math.floor(Math.random() * (range - i));
+                a.push(((r in m) ? m[r] : r) + 1);
+                let l = range - i - 1;
+                m[r] = (l in m) ? m[l] : l;
+            }
+            return a;
+           // return Math.floor(Math.random() * max);
+          }
+
+        setArrayRan(randomArray);
+        console.log(random(100));
     }
 
+    const [array, setArray] = useState([1, 2, 3, 4, 5]);
+      
+    const handleOnClickReverse = () => {
+        const newArray = reverse(array)
+        function reverse(arr){
 
-    console.log(reverse(array));
-    //console.log(array[0]);
-
-    // document.querySelector('randomArray').onclick = function() {
-    //     let result = [];
-    //     for(let i = 0; i < 1; i++) {
-    //         result.push(randomInteger(0, 6));
-    //     }
-    //     document.querySelector('#blockArray').innerHTML = result;
-    //     return result;
-    // }
-
-    // function randomInteger(min, max) {
-    //     let rand = min - 0.5 + Math.random() * (max - min + 1)
-    //     rand = Math.round(rand);
-    //     return rand;
-    // }
-
+            for ( let i=0; i <= (arr.length - 1) / 2 ; i++) {
+                const element = arr[i];
+                arr[i] = arr[arr.length - (i+1)];
+                arr[arr.length - (i+1)] = element;
+            }
+            return arr
+        }
+        setArray(newArray);
+        console.log(newArray);
+    }
 
     const getArrayContent = () => {
         return (array.map((item, index) => {
@@ -40,16 +50,18 @@ const ReverseArray = () => {
         }));
     }
 
+
+
     return (
         <div className={classes.reverseArrayContainer}>
             <div className={classes.blockArrayContainer}>
                 {getArrayContent()}
             </div>
             <div className={classes.randomButtonContainer}>
-                <button className={classes.randomArray}>Random Array</button>
+                <button className={classes.randomArray} onClick={handleOnClickRandom}>Randomise</button>
             </div>
             <div className={classes.reverseButtonContainer}>
-                <button className={classes.reverseArray} onClick={reverse}>Reverse Array</button>
+                <button className={classes.reverseArray} onClick={handleOnClickReverse}>Reverse Array</button>
             </div>
         </div>
     )
